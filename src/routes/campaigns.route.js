9682d22e42881ca165e5bd7767a3de96be8eeb62
tranthanhdao <tranthanhdao1311@gmail.com>
2023-05-20 13:47:46 +0700

    aaa

--- src/routes/campaigns.route.js
+++ src/routes/campaigns.route.js
@@ -5,8 +5,15 @@ const verifyToken = require("../middleware/auth");
 
 const Campaigns = require("../database/campaigns.model");
 //verifyToken
-router.get("/campaigns", verifyToken, async (req, res) => {
+router.get("/campaigns", async (req, res) => {
   const data = await Campaigns.find();
+
+  res.json(data);
+});
+
+router.post("/campaigns", async (req, res) => {
+  const data = new Campaigns(req.body);
+  await data.save();
 
   res.json(data);
 });
