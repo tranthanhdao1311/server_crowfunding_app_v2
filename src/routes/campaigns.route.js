38eea26771ce58e1bd2f4f7cbc5782de4b369728
tranthanhdao <tranthanhdao1311@gmail.com>
2023-05-23 03:30:57 +0700

    .

--- src/routes/campaigns.route.js
+++ src/routes/campaigns.route.js
@@ -22,8 +22,28 @@ router.post("/campaigns", async (req, res) => {
   res.json(data);
 });
 
-router.get("/campaigns/:id", async (req, res) => {
+router.put("/campaigns/:id", async (req, res) => {
   const { id } = req.params;
+
+  try {
+    const campaign = await Campaigns.findOneAndUpdate(
+      { id: id },
+      { ...req.body },
+      { new: true }
+    );
+
+    if (!campaign) {
+      return res.status(404).json({ error: "Campaign not found" });
+    }
+
+    res.json(campaign);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.get("/campaigns/:id", async (req, res) => {
+  const id = req.params.id;
 
   try {
     const data = await Campaigns.findOne({ id: id });
